df306618de6ff26cafb32d1df2e27200df267eea
emilx10 <51999563+emilx10@users.noreply.github.com>
2024-07-05 12:14:11 +0300

    Obstacles
    Added some scripts.

---
+++ Assets/Susita/Scripts/EntityDamage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class  EntityDamage : MonoBehaviour
+{
+    [SerializeField] protected float _damage;
+
+    PlayerHealth _playerHealth;
+
+    GameManager _gameManager;
+
+    private void Start()
+    {
+        _gameManager = GameManager.Instance;
+        _playerHealth = _gameManager.GetPlayerHealth();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            OnImpact();
+            _playerHealth.TakeDamage(_damage);
+        }
+    }
+
+    protected abstract void OnImpact();
+}
